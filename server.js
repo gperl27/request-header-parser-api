76e24c2b43b6fbf02a4096f0729ceb8c84eadb46
Greg Perlman <gperlman27@gmail.com>
2016-07-17 16:45:16 +0000

    final prototype

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+var ip = require('ip');
+
+var port = process.env.Port || 8080;
+
+//ip.address();
+
+app.get('/', function(req,res){
+    var language = req.headers["accept-language"].split(',');
+    var os = req.headers['user-agent'].split(') ')[0].split(' (')[1];
+    res.json({
+      'IP': ip.address(),
+      'Language' : language[0],
+      'OS' : os
+    });
+
+});
+
+app.listen(port, function(){
+  console.log('Listening on port ' + port);
+});
